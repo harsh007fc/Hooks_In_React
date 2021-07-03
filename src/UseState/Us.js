34e58da37c657455e33e6d76ad3ff3cc2c52e6f8
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-03 21:57:06 +0530

    code added upto spread optr ki ehmiat

--- src/UseState/Us.js
+++ src/UseState/Us.js
@@ -1,12 +1,12 @@
 import React,{useState} from 'react'
 
 function Us() {
-    let [msgObj,setMessage] = useState({message:''})
+    let [msgObj,setMessage] = useState({message:'',id:1})
     let handleChange = (e) =>{
         let val = e.target.value;
         // msgObj.message = val;
         // console.log(msgObj);
-        // setMessage({message:val});
+        // setMessage({message:val}); //iska yeh con ai ki agr multiple values hui toh update k baad sirf likhi hui value reh jayegi baaki saari gayab ho jjayegi isliyr hum h baar spread oprt use krte hain
         setMessage({...msgObj,message:val});
     }
         return (
